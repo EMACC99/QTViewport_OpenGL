815770944cf15cbc2544ac7741e65254da7ea8c8
emacc <lalitoceja@gmail.com>
2022-03-19 12:43:53 -0600

    new selection window

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
-/****************************************************************************
-https://www.youtube.com/watch?v=a2CpOQsgB84
-https://www.youtube.com/watch?v=6eu8oMULqaU
-Tutoriales de Parwiz Forogh
-****************************************************************************/
-
-#include "includes/openglwindow.hpp"
+#include "includes/select.hpp"
 #include <QApplication>
 
-int main(int argc, char **argv)
-{
-    QApplication a(argc,argv);
-    MainWindow w;
-    w.setTitle("OpenGL");
-    w.resize(640,480);
-    w.show();
+int main(int argc, char **argv){
 
-    return a.exec();
+    QApplication app(argc, argv);
+
+    ExampleSelect ui;
+    ui.show();
+
+    return app.exec();
 }
