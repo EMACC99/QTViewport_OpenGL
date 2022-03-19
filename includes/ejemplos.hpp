b328b3f68e8bc11e428468f7da5501bfe8c0be15
emacc <lalitoceja@gmail.com>
2022-03-19 13:10:49 -0600

    no anonymous namespace in header

--- includes/ejemplos.hpp
+++ includes/ejemplos.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <QMessageBox>
-
 #ifdef __APPLE__
     #include <OpenGL/glu.h>
 #else
@@ -9,15 +7,6 @@
 #endif
 
 namespace ejemplos {
-
-    namespace {
-        void ejemplo1_init();
-        void ejemplo1();
-        void ejemplo2_init();
-        void ejemplo2(int *list);
-        void ejemplo4_init();
-        void ejemplo4();
-    }
 
     void initialize_ejemplo(const int &ejemplo, int *list);
 
