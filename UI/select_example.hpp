ffe7faa57d37b010951bc429c8edef03c2c216e4
Eduardo Ceja <50022572+EMACC99@users.noreply.github.com>
2022-04-01 13:22:31 -0600

    Update select_example.hpp

--- UI/select_example.hpp
+++ UI/select_example.hpp
@@ -10,7 +10,7 @@
 #define SELECT_EXAMPLE_H
 
 #include <QtCore/QVariant>
-#include <QtGui/QAction>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QComboBox>
